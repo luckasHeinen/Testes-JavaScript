f4e0d6981c99ded1931d1701f504597fb7558613
Lucas Heinen <lucasheinen280502@gmail.com>
2026-08-30 20:51:19 -0300

    Exercicios da Aula 2

---
+++ Aula 2/Aula 2.js
@@ -0,0 +1,59 @@
+let tipoTexto = 'tipoTexto';
+let tipoNumero = 3.15;
+let verdadeiro = true;
+let falso = false;
+let tipoNulo = null;
+let tipoIndefinido;
+
+let nome = 'Harry Potter';
+let idade = 210;
+let souUmBruxo = true;
+
+let cidade = 'Parauapebas';
+cidade = 'Rio de Janeiro';
+
+const meuNome = 'Lucas';
+
+let minhaIdade = 24;
+
+let email = 'email@trybe.com';
+    email = 'outroEmail@trybe.com';
+
+let idade = 20;
+idade = 21;
+
+let tecnologia = 'HTML';
+tecnologia = 'JavaScript';
+
+let pontuacao = 0;
+pontuacao = 100;
+
+let canal = 'Youtube';
+canal = 'Twitch';
+
+let clima = 'Ensolarado';
+clima = 'chuvoso'
+
+let preco = 49.90;
+preco = 39.90
+
+let statusLog = false;
+statusLog= true;
+
+let idioma = 'inglês';
+idioma = 'português';
+
+let nivel = 1;
+nivel = 2;
+
+const PI = 3.14;
+const paisNascimento = 'Brasil';
+const anoNascimento = 2002;
+const cpf = '05738623037';
+const diasDaSemana = 7;
+const nomeCompleto = 'Lucas Silva Heinen';
+const horasDoDia = 24;
+const linguaMaterna = 'Português';
+const codigoGlobal = 'XYZ-123';
+const mesesDoAno = 12;
+
